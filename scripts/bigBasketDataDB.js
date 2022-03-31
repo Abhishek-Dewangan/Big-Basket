64fb71a554fe3e43a6e442cd36d292d1575f637f
Abinash Panigrahi <abinash24x7@gmail.com>
2022-03-31 09:39:36 +0530

    created eggs-meat html
    completed added all functionality

--- scripts/bigBasketDataDB.js
+++ scripts/bigBasketDataDB.js
@@ -451,3 +451,110 @@ let bestSellersDB = [
 
 
   localStorage.setItem("foodGrainsDB", JSON.stringify(foodGrainsDB));
+
+
+
+
+  let eggMeatFishDB = [
+    {
+      prod_name: "Farm Eggs - Table Tray, Medium, Antibiotic Residue-Free",
+      prod_price: "179",
+      prod_strike_price: "250",
+      prod_thumbnail: "https://www.bigbasket.com/media/uploads/p/s/150502_6-fresho-farm-eggs-table-tray-medium-antibiotic-residue-free.jpg",
+      prod_price_unit: "pcs",
+      prod_piece_unit: "30",
+      prod_brand: "Fresho",
+      prod_rating: "3.5",
+      prod_type: "nonveg"
+    },
+    {
+      prod_name: "Chicken - Curry Cut Without Skin, Antibiotic Residue-Free",
+      prod_price: "290",
+      prod_strike_price: "400",
+      prod_thumbnail: "https://www.bigbasket.com/media/uploads/p/s/40048898_5-fresho-chicken-curry-cut-without-skin-antibiotic-residue-free.jpg",
+      prod_price_unit: "kg",
+      prod_piece_unit: "",
+      prod_brand: "Fresho",
+      prod_rating: "3.3",
+      prod_type: "nonveg"
+    },
+    {
+      prod_name: "Eggs - Regular",
+      prod_price: "84",
+      prod_strike_price: "110",
+      prod_thumbnail: "https://www.bigbasket.com/media/uploads/p/s/1203477_2-fresho-eggs-regular.jpg",
+      prod_price_unit: "pcs",
+      prod_piece_unit: "12",
+      prod_brand: "Fresho",
+      prod_rating: "3.7",
+      prod_type: "nonveg"
+    },
+    {
+      prod_name: "Fresho Farm Eggs Medium 30 pcs + Kara Coconut Milk UHT Classic, Imported 200 ml",
+      prod_price: "245",
+      prod_strike_price: "320",
+      prod_thumbnail: "https://www.bigbasket.com/media/uploads/p/s/1213737_1-bb-combo-fresho-farm-eggs-medium-30-pcs-kara-coconut-milk-uht-classic-imported-200-ml.jpg",
+      prod_price_unit: "pcs",
+      prod_piece_unit: "1",
+      prod_brand: "BB Combo",
+      prod_rating: "3.8",
+      prod_type: "nonveg"
+    },
+    {
+      prod_name: "Rohu Fish - Large, Curry Cut/Bengali Cut, Preservative Free, 12-16 pcs",
+      prod_price: "250",
+      prod_strike_price: "410",
+      prod_thumbnail: "https://www.bigbasket.com/media/uploads/p/s/10000756_34-fresho-rohu-fish-large-curry-cutbengali-cut-preservative-free-12-15-pcs.jpg",
+      prod_price_unit: "kg",
+      prod_piece_unit: "",
+      prod_brand: "Fresho",
+      prod_rating: "3.5",
+      prod_type: "nonveg"
+    },
+    {
+      prod_name: "Fresho Chicken Breast Boneless 4-5 pcs 500g +Kara Coconut Milk UHT Classic 200ml",
+      prod_price: "295",
+      prod_strike_price: "340",
+      prod_thumbnail: "https://www.bigbasket.com/media/uploads/p/s/1213733_1-bb-combo-fresho-chicken-breast-boneless-4-5-pcs-500g-kara-coconut-milk-uht-classic-200ml.jpg",
+      prod_price_unit: "kg",
+      prod_piece_unit: "",
+      prod_brand: "BB Combo",
+      prod_rating: "3.8",
+      prod_type: "nonveg"
+    },
+    {
+      prod_name: "River Salmon - Boneless Curry Cut, Preservative Free, 10-15 pcs",
+      prod_price: "649",
+      prod_strike_price: "715",
+      prod_thumbnail: "https://www.bigbasket.com/media/uploads/p/s/20001407_1-fresho-river-salmon-boneless-curry-cut-preservative-free-10-15-pcs.jpg",
+      prod_price_unit: "kg",
+      prod_piece_unit: "",
+      prod_brand: "Fresho",
+      prod_rating: "3.3",
+      prod_type: "nonveg"
+    },
+    {
+      prod_name: "Kolkata Tengra Fish Whole Uncleaned",
+      prod_price: "259",
+      prod_strike_price: "350",
+      prod_thumbnail: "https://www.bigbasket.com/media/uploads/p/s/40248644_2-fresho-tengra-fish-whole-uncleaned-fresh-flavourful.jpg",
+      prod_price_unit: "kg",
+      prod_piece_unit: "",
+      prod_brand: "Fresho",
+      prod_rating: "3.4",
+      prod_type: "nonveg"
+    },
+    {
+      prod_name: "Chicken Drumstick - Without Skin, Antibiotic Residue-Free",
+      prod_price: "329",
+      prod_strike_price: "420",
+      prod_thumbnail: "https://www.bigbasket.com/media/uploads/p/s/102592_8-fresho-chicken-drumstick-without-skin-antibiotic-residue-free.jpg",
+      prod_price_unit: "kg",
+      prod_piece_unit: "",
+      prod_brand: "Fresho",
+      prod_rating: "3.6",
+      prod_type: "nonveg"
+    }
+  ]
+
+  localStorage.setItem("eggMeatFishDB", JSON.stringify(eggMeatFishDB));
