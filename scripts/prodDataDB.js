db2061a76a83022ca9daa9eaceceb399ec6649dc
Abinash Panigrahi <abinash24x7@gmail.com>
2022-04-03 15:54:43 +0530

    abinash part added

---
+++ scripts/prodDataDB.js
@@ -0,0 +1,451 @@
+let prodData = 
+[
+    {
+        "prod_name": "Onion",
+        "prod_price": "100",
+        "prod_strike_price": "150",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/40050957_2-fresho-onion-organically-grown.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "Fresho",
+        "prod_rating": "3.7"
+    },
+    {
+        "prod_name": "Coriander",
+        "prod_price": "12",
+        "prod_strike_price": "15",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/10000097_19-fresho-coriander-leaves.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "Fresho",
+        "prod_rating": "4"
+    },
+    {
+        "prod_name": "Banana",
+        "prod_price": "102",
+        "prod_strike_price": "150",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/10000031_21-fresho-banana-yelakki.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "fruits",
+        "prod_brand": "Fresho",
+        "prod_rating": "4"
+    },
+    {
+        "prod_name": "Green Peas Fresh",
+        "prod_price": "94",
+        "prod_strike_price": "117",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/10000284_11-fresho-green-peas.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "Fresho",
+        "prod_rating": "4.1"
+    },
+    {
+        "prod_name": "Apple",
+        "prod_price": "126",
+        "prod_strike_price": "171",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/40033823_20-fresho-apple-royal-gala-regular.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "fruits",
+        "prod_brand": "Fresho",
+        "prod_rating": "4.6"
+    },
+    {
+        "prod_name": "Cucumber",
+        "prod_price": "30",
+        "prod_strike_price": "41",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/30007400_4-fresho-cucumber.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "fruits",
+        "prod_brand": "Fresho",
+        "prod_rating": "4.6"
+    },
+    {
+        "prod_name": "Beetroot",
+        "prod_price": "28",
+        "prod_strike_price": "39",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/10000047_21-fresho-beetroot.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "fruits",
+        "prod_brand": "Fresho",
+        "prod_rating": "3.8"
+    },
+    {
+        "prod_name": "Orange",
+        "prod_price": "126",
+        "prod_strike_price": "157",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/10000266_27-fresho-orange-nagpur-regular.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "fruits",
+        "prod_brand": "Fresho",
+        "prod_rating": "3.8"
+    },
+    {
+        "prod_name": "Watermelon",
+        "prod_price": "50",
+        "prod_strike_price": "62",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/10000207_21-fresho-watermelon-small.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "fruits",
+        "prod_brand": "Fresho",
+        "prod_rating": "4.1"
+    },
+    {
+        "prod_name": "Tomato",
+        "prod_price": "29",
+        "prod_strike_price": "36",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/10000200_17-fresho-tomato-hybrid.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "Fresho",
+        "prod_rating": "3.8"
+    },
+    {
+        "prod_name": "Potato",
+        "prod_price": "25",
+        "prod_strike_price": "35",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/40048457_7-fresho-potato-new-crop.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "Fresho",
+        "prod_rating": "3.7"
+    },
+    {
+        "prod_name": "Cauliflower",
+        "prod_price": "19",
+        "prod_strike_price": "23",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/10000074_19-fresho-cauliflower.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "Fresho",
+        "prod_rating": "3.7"
+    },
+    {
+        "prod_name": "Ladis Finger",
+        "prod_price": "11",
+        "prod_strike_price": "14",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/10000143_14-fresho-ladies-finger.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "Fresho",
+        "prod_rating": "4.3"
+    },
+    {
+        "prod_name": "Graps",
+        "prod_price": "50",
+        "prod_strike_price": "62",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/40218332_1-fresho-grapes-green.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "fruits",
+        "prod_brand": "Fresho",
+        "prod_rating": "3.7"
+    },
+    {
+        "prod_name": "Pomegranate",
+        "prod_price": "186",
+        "prod_strike_price": "232",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/10000269_30-fresho-pomegranate.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "fruits",
+        "prod_brand": "Fresho",
+        "prod_rating": "4.6"
+    },
+    {
+        "prod_name": "Bringel",
+        "prod_price": "50",
+        "prod_strike_price": "80",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/30000435_12-fresho-brinjal-purple-bharta.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "Fresho",
+        "prod_rating": "3.7"
+    },
+    {
+        "prod_name": "Ginger",
+        "prod_price": "16",
+        "prod_strike_price": "20",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/40023480_4-fresho-ginger-organically-grown.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "Fresho",
+        "prod_rating": "4.3"
+    },
+    {
+        "prod_name": "Kiwi",
+        "prod_price": "89",
+        "prod_strike_price": "111",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/20000911_30-fresho-kiwi-green.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "fruits",
+        "prod_brand": "Fresho",
+        "prod_rating": "4.3"
+    },
+    {
+        "prod_name": "Basmati Rice",
+        "prod_price": "101",
+        "prod_strike_price": "124",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/1203724_1-bb-combo-bb-royal-basmati-rice-everyday-1-kg-fresho-new-potato-1-kg.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "foodgrains",
+        "prod_brand": "bbcombo",
+        "prod_rating": "4.3"
+    },
+    {
+        "prod_name": "Refind Sugar",
+        "prod_price": "225",
+        "prod_strike_price": "300",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/10000407_12-bb-royal-refined-sugar-sulphurless.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "foodgrains",
+        "prod_brand": "bbcombo",
+        "prod_rating": "3.8"
+    },
+    {
+        "prod_name": "Toor Daal",
+        "prod_price": "130",
+        "prod_strike_price": "174",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/mm/40000291_8-tata-sampann-unpolished-toor-dalarhar-dal.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "foodgrains",
+        "prod_brand": "bbcombo",
+        "prod_rating": "4.6"
+    },
+    {
+        "prod_name": "Atta",
+        "prod_price": "217",
+        "prod_strike_price": "255",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/bpi/mumbai/mm/126903_1-aashirvaad-atta-whole-wheat.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "foodgrains",
+        "prod_brand": "bbcombo",
+        "prod_rating": "3.8"
+    },
+    {
+        "prod_name": "Olive Oil",
+        "prod_price": "1373",
+        "prod_strike_price": "2850",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/mm/1212929_1-borges-olive-oil-extra-light-2-l-bottle-durum-wheat-pasta-penne-rigate-500-g-pouch.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "oil",
+        "prod_brand": "bbcombo",
+        "prod_rating": "4.1"
+    },
+    {
+        "prod_name": "Sooji",
+        "prod_price": "33",
+        "prod_strike_price": "37",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/mm/289910_10-madam-sooji-rawa.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "foodgrains",
+        "prod_brand": "bbcombo",
+        "prod_rating": "4.3"
+    },
+    {
+        "prod_name": "Cow Ghee",
+        "prod_price": "988",
+        "prod_strike_price": "1040",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/mm/1214166_1-parsi-dairy-farm-farm-cow-ghee.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "oil",
+        "prod_brand": "bbcombo",
+        "prod_rating": "4.3"
+    },
+    {
+        "prod_name": "Cooking Oil",
+        "prod_price": "211",
+        "prod_strike_price": "225",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/mm/147491_8-saffola-gold-refined-cooking-oil-blended-rice-bran-sunflower-oil-helps-keeps-heart-healthy.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "oil",
+        "prod_brand": "bbcombo",
+        "prod_rating": "4.6"
+    },
+    {
+        "prod_name": "Mastered Oil",
+        "prod_price": "213",
+        "prod_strike_price": "233",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/mm/276756_8-fortune-fortune-premium-kachi-ghani-pure-mustard-oil.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "oil",
+        "prod_brand": "bbcombo",
+        "prod_rating": "4"
+    },
+    {
+        "prod_name": "Sunflower Oil",
+        "prod_price": "204",
+        "prod_strike_price": "225",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/mm/40018396_4-sunrich-refined-sunflower-oil.jpg",
+        "prod_quantity": "1",
+        "prod_price_unit": "kg",
+        "prod_category": "oil",
+        "prod_brand": "bbcombo",
+        "prod_rating": "4"
+    },
+    {
+        "prod_name": "Garam Masala",
+        "prod_price": "68",
+        "prod_strike_price": "80",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/268943_3-everest-garam-masala.jpg",
+        "prod_quantity": "250",
+        "prod_price_unit": "gm",
+        "prod_category": "mashala",
+        "prod_brand": "bbcombo",
+        "prod_rating": "4.6"
+    },
+    {
+        "prod_name": "Chicken Masala",
+        "prod_price": "37",
+        "prod_strike_price": "39",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/268076_2-everest-chicken-masala.jpg",
+        "prod_quantity": "250",
+        "prod_price_unit": "gm",
+        "prod_category": "mashala",
+        "prod_brand": "bbcombo",
+        "prod_rating": "4"
+    },
+    {
+        "prod_name": "Kitchen King Masala",
+        "prod_price": "66",
+        "prod_strike_price": "70",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/268942_3-everest-kitchen-king-masala.jpg",
+        "prod_quantity": "250",
+        "prod_price_unit": "gm",
+        "prod_category": "mashala",
+        "prod_brand": "bbcombo",
+        "prod_rating": "3.7"
+    },
+    {
+        "prod_name": "Maggie Masala",
+        "prod_price": "52",
+        "prod_strike_price": "58",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/40123264_9-maggi-masala-ae-magic-seasoning-vegetable-masala.jpg",
+        "prod_quantity": "250",
+        "prod_price_unit": "gm",
+        "prod_category": "mashala",
+        "prod_brand": "bbcombo",
+        "prod_rating": "3.7"
+    },
+    {
+        "prod_name": "Brown Bread",
+        "prod_price": "42",
+        "prod_strike_price": "55",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/40082738_1-the-bake-shop-whole-wheat-brown-bread-with-bran.jpg",
+        "prod_quantity": "500",
+        "prod_price_unit": "gm",
+        "prod_category": "bakery",
+        "prod_brand": "Brotos",
+        "prod_rating": "4.3"
+    },
+    {
+        "prod_name": "White Butter",
+        "prod_price": "456",
+        "prod_strike_price": "480",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/30009708_2-parsi-dairy-farm-butter-white.jpg",
+        "prod_price_unit": "gm",
+        "prod_category": "bakery",
+        "prod_brand": "Amul",
+        "prod_rating": "4"
+    },
+    {
+        "prod_name": "Fresh Milk",
+        "prod_price": "35",
+        "prod_strike_price": "40",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/40041431_1-amul-taaza-fresh-toned-milk.jpg",
+        "prod_price_unit": "gm",
+        "prod_category": "bakery",
+        "prod_brand": "Amul",
+        "prod_rating": "4.3"
+    },
+    {
+        "prod_name": "Dahi",
+        "prod_price": "77",
+        "prod_strike_price": "80",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/1213630_1-amul-masti-dahi.jpg",
+        "prod_price_unit": "gm",
+        "prod_category": "bakery",
+        "prod_brand": "Amul",
+        "prod_rating": "3.7"
+    },
+    {
+        "prod_name": "Cookie",
+        "prod_price": "120",
+        "prod_strike_price": "130",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/s/40250008_1-eighth-sin-cookie-bites-butter-soft-crisp-buttery-yummy-snack.jpg",
+        "prod_price_unit": "gm",
+        "prod_category": "bakery",
+        "prod_brand": "Britannia",
+        "prod_rating": "4.6"
+    },
+    {
+        "prod_name": "Fresh Cream",
+        "prod_price": "205",
+        "prod_strike_price": "220",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/mm/40005030_2-amul-fresh-cream-25-milk-fat-low-fat.jpg",
+        "prod_price_unit": "kg",
+        "prod_category": "bakery",
+        "prod_brand": "Amul",
+        "prod_rating": "3.8"
+    },
+    {
+        "prod_name": "Fresho Apple-Red Delicious/Washington 4pcs+BB Home Vegetable & Fruit Wash 500ml",
+        "prod_price": "324",
+        "prod_strike_price": "365",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/mm/1212557_1-bb-combo-fresho-apple-red-deliciouswashington-4pcsbb-home-vegetable-fruit-wash-500ml.jpg",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "BB Combo",
+        "prod_rating": "4.6"
+    },
+    {
+        "prod_name": "Fresho Bitter Gourd 500 g + BB Home Vegetable & Fruit Wash 500 ml",
+        "prod_price": "199",
+        "prod_strike_price": "209",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/mm/1212556_1-bb-combo-fresho-bitter-gourd-500-g-bb-home-vegetable-fruit-wash-500-ml.jpg",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "BB Combo",
+        "prod_rating": "3.8"
+    },
+    {
+        "prod_name": "Fresho Lettuce - Iceberg 500 g + BB Home Vegetable & Fruit Wash 500 ml",
+        "prod_price": "224",
+        "prod_strike_price": "250",
+        "prod_thumbnail": "https://www.bigbasket.com/media/uploads/p/mm/1212560_1-bb-combo-fresho-lettuce-iceberg-500-g-bb-home-vegetable-fruit-wash-500-ml.jpg",
+        "prod_price_unit": "kg",
+        "prod_category": "vegetables",
+        "prod_brand": "BB Combo",
+        "prod_rating": "4.1"
+    }
+];
+
+
+
+
+localStorage.setItem("product", JSON.stringify(prodData))
